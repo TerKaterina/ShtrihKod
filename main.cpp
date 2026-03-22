5096b3148be1be0e9f97da11e300630e69f28584
Ktototam228 <124391607+Ktototam228@users.noreply.github.com>
2026-03-23 01:26:04 +0300

    main.cpp || Убрал не работающую локализацию

--- main.cpp
+++ main.cpp
@@ -3,24 +3,7 @@
 #include "Sum.h"
 #include "Tests.h"
 
-#ifdef _WIN32
-    #include <windows.h>
-#endif
-
-void setRussianLocale() {
-    #ifdef _WIN32
-        // Для Windows
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
-        setlocale(LC_ALL, "Russian");
-    #else
-        // Для Linux/Mac
-        setlocale(LC_ALL, "ru_RU.UTF-8");
-    #endif
-}
-
 int main() {
-    setRussianLocale();
     
     Tests_headers Test_Sum_Header;
 	Test_Sum_Header.Sum_header();
