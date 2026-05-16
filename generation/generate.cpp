1399aaeb6849e84f0475f691653e44587f1aec19
Ktototam228 <voejkovila@gmail.com>
2026-05-16 17:09:56 +0300

    generation/generate.cpp(.hpp) || Откат изменений

--- generation/generate.cpp
+++ generation/generate.cpp
@@ -1,50 +1,43 @@
 #include "generate.hpp"
 
-cv::Mat render_code(ZXing::MultiFormatWriter& writer, std::string& data)
-{
-    auto matrix = writer.encode(data, 300, 100);
-    
-    int SCALE = 20;
-    int imgWidth = matrix.width() * SCALE;
-    int imgHeight = matrix.height() * SCALE;
-    
-    cv::Mat barcodeImage(imgHeight, imgWidth, CV_8UC3, cv::Scalar(255, 255, 255));
-    
-    for (int y = 0; y < matrix.height(); ++y) {
-        for (int x = 0; x < matrix.width(); ++x) {
-            if (matrix.get(x, y)) {
-                cv::rectangle(barcodeImage,
-                    cv::Point(x * SCALE, y * SCALE),
-                    cv::Point((x + 1) * SCALE, (y + 1) * SCALE),
-                    cv::Scalar(0, 0, 0),
-                    cv::FILLED);
-            }
-        }
-    }
-    
-    return barcodeImage;
-}
+void generate(std::string data) {
+	try {
+		ZXing::MultiFormatWriter writer(ZXing::BarcodeFormat::Code128);
 
-void output_file(cv::Mat& barcodeImage, std::string& data)
-{
-    std::string outputFilename = "barcode.png";
-    cv::imwrite(outputFilename, barcodeImage);
-    std::cout << "Штрихкод успешно сохранен в файл: " << outputFilename << std::endl;
-}
+		writer.setEncoding(ZXing::CharacterSet::UTF8);
+		writer.setMargin(20);
 
-void generate(std::string& data) 
-{
-    try {
-		// конструктор
-        ZXing::MultiFormatWriter writer(ZXing::BarcodeFormat::Code128);
+		//std::string data = "1234567890";
+
+		auto matrix = writer.encode(data, 300, 100);
+
+		const int SCALE = 20;
+
+		int imgWidth = matrix.width() * SCALE;
+        	int imgHeight = matrix.height() * SCALE;
+        	cv::Mat barcodeImage(imgHeight, imgWidth, CV_8UC3, cv::Scalar(255, 255, 255)); // Белый фон
         
-        // генерация и рендеринг
-        cv::Mat barcodeImage = render_code(writer, data);
-        
-        // сохранение
-        output_file(barcodeImage, data);
-        
-    } catch (std::exception& e) {
-        std::cerr << "Ошибка: " << e.what() << std::endl;
-    }
+        	for (int y = 0; y < matrix.height(); ++y) {
+                	for (int x = 0; x < matrix.width(); ++x) {
+                    		if (matrix.get(x, y)) {
+					cv::rectangle(barcodeImage,
+							cv::Point(x * SCALE, y * SCALE),
+							cv::Point((x + 1) * SCALE, (y + 1) * SCALE),
+							cv::Scalar(0, 0, 0),
+							cv::FILLED);
+                		}
+            		}
+        	}
+		std::string outputFilename = "barcode.png";
+		cv::imwrite(outputFilename, barcodeImage);
+		std::cout << "The barcode has been successfully created in file " << outputFilename << std::endl;
+
+		// Опционально: показать изображение в окне
+		// cv::imshow("Сгенерированный штрихкод", barcodeImage);
+		// cv::waitKey(0);
+
+
+	} catch (const std::exception& e) {
+		std::cerr << "Ошибка: " << e.what() << std::endl;
+	}
 }
--- generation/generate.hpp
+++ generation/generate.hpp
@@ -1,16 +1,12 @@
-#ifndef GENERATE_HPP
-#define GENERATE_HPP
+#ifndef GENERATE
+#define GENERATE
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <ZXing/MultiFormatWriter.h>
 #include <ZXing/BitMatrix.h>
-#include <ZXing/BarcodeFormat.h>
 #include <ZXing/CharacterSet.h>
-#include <string>
 
-cv::Mat render_code(ZXing::MultiFormatWriter& writer, std::string& data);
-void output_file(cv::Mat& barcodeImage, std::string& data);
-void generate(std::string& data);
+void generate(std::string data);
 
-#endif //GENERATE_HPP
+#endif //GENERATE
