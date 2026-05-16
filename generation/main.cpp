ce8094b476677709215256cf0b35dc78fdad736a
Ktototam228 <voejkovila@gmail.com>
2026-05-16 17:12:17 +0300

    generation/main.cpp || Добавление ввода перед закрытием

--- generation/main.cpp
+++ generation/main.cpp
@@ -1,8 +1,13 @@
 #include "generate.hpp"
+#include <iostream>
 
 int main() {
-	//std::string code = "1234567890";
+	
 	std::string code = "hello";
 	generate(code);
+
+
+	char n;
+	std::cin >> n;
 	return 0;
 }
